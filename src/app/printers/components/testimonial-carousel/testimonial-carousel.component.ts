3d91d868f694890db7d41afc835ccc07c0cf9991
Billy Alvear <alvearbilly@gmail.com>
2023-10-07 15:22:43 -0500

    espaciado-tech-serv

--- src/app/printers/components/testimonial-carousel/testimonial-carousel.component.ts
+++ src/app/printers/components/testimonial-carousel/testimonial-carousel.component.ts
@@ -42,7 +42,7 @@ export class TestimonialCarouselComponent {
       job: 'Gerente de Sistemas'
     },
     {
-      score: 5,
+      score: 4.5,
       title: 'Excelente Servicio',
       prg: 'Amet reprehenderit ut do consectetur non sit consequat elit exercitation voluptate. Reprehenderit ullamco proident incididunt quis pariatur Lorem anim do id cupidatat cillum culpa. Pariatur sint dolore aute irure anim deserunt do enim non ea occaecat labore. Ut duis quis irure velit.',
       img: 'https://imageio.forbes.com/specials-images/imageserve/5bb22ae84bbe6f67d2e82e05/0x0.jpg?format=jpg&crop=1012,1013,x627,y129,safe&height=416&width=416&fit=bounds',
@@ -58,7 +58,7 @@ export class TestimonialCarouselComponent {
       job: 'Gerente de Sistemas'
     },
     {
-      score: 5,
+      score: 3.5,
       title: 'Excelente Servicio',
       prg: 'Amet reprehenderit ut do consectetur non sit consequat elit exercitation voluptate. Reprehenderit ullamco proident incididunt quis pariatur Lorem anim do id cupidatat cillum culpa. Pariatur sint dolore aute irure anim deserunt do enim non ea occaecat labore. Ut duis quis irure velit.',
       img: 'https://imageio.forbes.com/specials-images/imageserve/5bb22ae84bbe6f67d2e82e05/0x0.jpg?format=jpg&crop=1012,1013,x627,y129,safe&height=416&width=416&fit=bounds',
@@ -66,7 +66,7 @@ export class TestimonialCarouselComponent {
       job: 'Gerente de Sistemas'
     },
     {
-      score: 5,
+      score: 4,
       title: 'Excelente Servicio',
       prg: 'Amet reprehenderit ut do consectetur non sit consequat elit exercitation voluptate. Reprehenderit ullamco proident incididunt quis pariatur Lorem anim do id cupidatat cillum culpa. Pariatur sint dolore aute irure anim deserunt do enim non ea occaecat labore. Ut duis quis irure velit.',
       img: 'https://imageio.forbes.com/specials-images/imageserve/5bb22ae84bbe6f67d2e82e05/0x0.jpg?format=jpg&crop=1012,1013,x627,y129,safe&height=416&width=416&fit=bounds',
