8adff4fc37f326749d4cd3fd3be0a371999d488e
Billy Alvear <alvearbilly@gmail.com>
2023-10-19 10:31:53 -0500

    more-details

--- src/app/printers/components/printer-carousel/printer-carousel.component.ts
+++ src/app/printers/components/printer-carousel/printer-carousel.component.ts
@@ -30,12 +30,12 @@ export class PrinterCarouselComponent implements OnInit{
     {
       breakpoint: '1024px',
       numVisible: 3,
-      numScroll: 3
+      numScroll: 1
     },
     {
         breakpoint: '768px',
         numVisible: 2,
-        numScroll: 2
+        numScroll: 1
     },
     {
         breakpoint: '576px',
