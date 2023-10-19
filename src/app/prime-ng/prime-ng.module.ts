f441786ae7ef37efa5b78b8aa7835e4f5b21ac54
Billy Alvear <alvearbilly@gmail.com>
2023-10-19 18:20:15 -0500

    refactoring

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { CarouselModule } from 'primeng/carousel';
-import { DialogModule } from 'primeng/dialog';
-import { ButtonModule } from 'primeng/button';
-
 
 @NgModule({
   exports: [
-    CarouselModule,
-    DialogModule,
-    ButtonModule
+    CarouselModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/printers/components/printer-card/printer-card.component.ts
+++ src/app/printers/components/printer-card/printer-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Printer } from '../../interfaces/printer.interface';
 import { ComunicacionService } from '../../services/comunicacion.service';
 
